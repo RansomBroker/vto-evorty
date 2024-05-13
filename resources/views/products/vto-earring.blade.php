7f073f1e3bfeee77daf3a8b360949efc4725bfc6
RansomBroker <yadisoke@gmail.com>
2024-05-14 02:44:16 +0700

    selesai tampilan baru earring

--- resources/views/products/vto-earring.blade.php
+++ resources/views/products/vto-earring.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="content-language" content="en-EN" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
 
-    <title>WebAR.rocks.face 3D earrings demo</title>
+    <title>VTO Earring</title>
 
     <!-- INCLUDE WebAR.rocks.face MAIN SCRIPT -->
     <script src="{{ asset('assets/webarrock/dist/WebARRocksFace.js') }}"></script>
@@ -28,8 +30,10 @@
     <script src="{{ asset('assets/webarrock/libs/three/v136/examples/js/shaders/CopyShader.js') }}"></script>
 
     <!-- Bloom postprocessing: -->
-    <script src="{{ asset('assets/webarrock/libs/three/v136/examples/js/postprocessing/UnrealBloomPassTweaked.js') }}"></script>
-    <script src="{{ asset('assets/webarrock/libs/three/v136/examples/js/shaders/LuminosityHighPassShader.js') }}"></script>
+    <script src="{{ asset('assets/webarrock/libs/three/v136/examples/js/postprocessing/UnrealBloomPassTweaked.js') }}">
+    </script>
+    <script src="{{ asset('assets/webarrock/libs/three/v136/examples/js/shaders/LuminosityHighPassShader.js') }}">
+    </script>
 
     <!-- TAA specifics: -->
     <script src="{{ asset('assets/webarrock/libs/three/v136/examples/js/postprocessing/SSAARenderPass.js') }}"></script>
@@ -49,8 +53,11 @@
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- owl carousel css -->
@@ -69,48 +76,69 @@
     <!-- END OFF ASSET EVORTY-->
 
 </head>
-<body class="bg-cosmic-latte w-full min-vh-100 overflow-auto d-flex align-items-center">
-<input type="hidden" name="product" value="{{ $product->id }}">
 
-<div class="container-fluid">
-    <div class="row justify-content-center m-0">
-        <!-- canvas-->
-        <div class="col-lg-6 col-md-6 col-12">
-            <div class="card bg-transparent border-0 rounded-lg">
-                <div class="card-body card-vto-container p-0">
-                    <div id='canvases' class="position-relative">
-                        <!-- canvas where the earring will be displayed: -->
-                        <canvas  id='VTOCanvas'></canvas>
-                        <!-- canvas where the video will be displayed: -->
-                        <canvas  id='faceTrackerCanvas'></canvas>
+<body class="bg-cosmic-latte w-full overflow-auto d-flex align-items-center">
+    <input type="hidden" name="product" value="{{ $product->id }}">
+
+    <div class="container-fluid container-control p-0">
+        <div class="row justify-content-center m-0">
+            <!-- canvas-->
+            <div class="col-lg-12 col-md-12 col-12 p-0">
+                <div class="card bg-transparent border-0 rounded-lg">
+                    <div class="card-body card-vto-container p-0">
+                        <div id='loading' class='modalVTO fade'>
+                            <div>
+                                LOADING...
+                            </div>
+                        </div>
+                        <div id='canvases' class="position-relative">
+                            <canvas id='VTOCanvas'></canvas>
+                            <canvas id='faceTrackerCanvas'></canvas>
+                        </div>
+                        <!-- model load -->
+                        <div class="col-lg-12 col-md-12 col-12 p-0 m-0 card-material-control">
+                            <div class="card border-0">
+                                <div class="card-body p-0 card-material">
+                                    <!-- nav -->
+                                    <nav class="px-md-4 px-2 pt-2 nav nav-pills mb-2 carousel-material-list owl-carousel owl-theme border-bottom"
+                                        id="materialTab" role="tablist">
+
+                                    </nav>
+                                    <div class="tab-content px-md-4 px-2 mb-1" id="pillsMaterialList">
+
+                                    </div>
+                                    <!-- product desc -->
+                                    <div class="d-flex justify-content-start mx-2">
+                                        <!-- back -->
+                                        <a href="{{ route('bracelet.vto.all', $brand->slug)}}" class="btn btn-back">
+                                            <i class='bx bxs-chevron-left'></i>
+                                        </a>
+                                        <div class="product-container d-flex justify-content-start">
+                                            <!-- image -->
+                                            <img class="ml-2 img-thumbnail-product"
+                                                src="{{ asset($product->base_folder.'/'.$product->thumbnail) }}" alt="">
+                                            <!-- Text -->
+                                            <p class="ml-1 pt-1 text-product">{{ $product->name }}</p>
+                                        </div>
+                                        <!-- shop now button -->
+                                        <div class="d-flex align-items-center">
+                                            <button class="btn-shop btn-sm btn btn-dark"> Shop Now</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <!-- model load -->
-        <div class="col-lg-6 col-md-6 col-12 mt-3 mt-lg-0">
-            <div class="border-0 border-cosmic mb-3"></div>
-            <div class="card bg-transparent border-0 rounded-lg">
-                <div class="card-body">
-                    <ul class="nav nav-tabs mb-3 border-cosmic" id="materialTab" role="tablist">
-
-                    </ul>
-                    <div class="tab-content" id="pillsMaterialList">
-
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
     </div>
-</div>
 
-<script>
+    <script>
     const BASE_URL = "{{ url('/') }}"
-</script>
-<!-- INCLUDE DEMO SCRIPT -->
-<script src="{{ asset('assets/webarrock/js/earring.js') }}"></script>
+    </script>
+    <!-- INCLUDE DEMO SCRIPT -->
+    <script src="{{ asset('assets/webarrock/js/earring.js') }}"></script>
 </body>
+
 </html>
