0adbe619fd36f2cc2d67a2643ef9b98b5817ea67
RansomBroker <yadisoke@gmail.com>
2024-05-15 19:12:41 +0700

    selesai css vto all bracelet,watch

--- resources/views/products/vto-watch-all.blade.php
+++ resources/views/products/vto-watch-all.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="content-language" content="en-EN" />
@@ -12,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet">
+        rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
@@ -33,30 +34,31 @@
     <!-- END OFF ASSET EVORTY-->
 </head>
 
-<body class="bg-cosmic-latte w-full overflow-auto d-flex align-items-center">
-<input type="hidden" name="brand" value="{{ $brand->name }}">
+<body class="bg-dark-alpha">
+    <input type="hidden" name="brand" value="{{ $brand->name }}">
 
-<div class="container-fluid container-control p-0">
-    <div class="row justify-content-center m-0">
-        <!-- canvas-->
-        <div class="col-lg-12 col-md-12 col-12 p-0">
-            <div class="card bg-transparent border-0 rounded-lg">
-                <div class="card-body card-vto-container p-0">
-                    <div id='canvases' class="position-relative">
-                        <canvas id='camera'></canvas>
-                    </div>
-                    <!-- model load -->
-                    <div class="col-lg-12 col-md-12 col-12 p-0 m-0 card-product-control">
-                        <div class="card border-0">
-                            <div class="card-body card-product">
-                                <!-- nav -->
-                                <div class="tab-content owl-carousel owl-theme" id="pillsProductList">
-                                    @foreach($products as $product)
+    <div class="container-fluid p-0">
+        <div class="row justify-content-center m-0 container-vto d-flex align-items-center">
+            <!-- canvas-->
+            <div class="col-lg-8 col-md-12 col-12 p-0">
+                <div class="card bg-transparent border-0 rounded-lg">
+                    <div class="card-body card-vto-container p-0">
+                        <div id='canvases' class="position-relative">
+                            <canvas id='camera'></canvas>
+                        </div>
+                        <!-- model load -->
+                        <div class="col-lg-12 col-md-12 col-12 p-0 m-0 card-product-control">
+                            <div class="card border-0 rounded-0">
+                                <div class="card-body card-product">
+                                    <!-- nav -->
+                                    <div class="tab-content owl-carousel owl-theme" id="pillsProductList">
+                                        @foreach($products as $product)
                                         <a href="{{ route('watch.vto', [$brand->slug, $product->slug]) }}">
                                             <img src="{{ asset($product->base_folder.'/'.$product->thumbnail) }}" alt=""
-                                                 class="card-item-product">
+                                                class="card-item-product">
                                         </a>
-                                    @endforeach
+                                        @endforeach
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -65,12 +67,11 @@
             </div>
         </div>
     </div>
-</div>
 
-<script>
+    <script>
     const BASE_URL = "{{ url('/') }}"
-</script>
-<script src="{{ asset('assets/webarrock/js/watch-all-product.js') }}"></script>
+    </script>
+    <script src="{{ asset('assets/webarrock/js/watch-all-product.js') }}"></script>
 </body>
 
 </html>
